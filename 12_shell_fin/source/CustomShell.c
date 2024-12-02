0a07a107a1f79da569adef2e3dc31814b1c504a6
swatper <swatper@swatper>
2024-12-02 19:42:35 +0900

    1차

--- 12_shell_fin/source/CustomShell.c
+++ 12_shell_fin/source/CustomShell.c
@@ -30,19 +30,17 @@
 // ----------------------------------------------------
 // SIGINT 핸들러 (Ctrl-C)
 // ----------------------------------------------------
-void handle_sigint(int sig)
-{
+void handle_sigint_parent(int sig) {
+    printf("\nSIGINT 수신 - 작업 중단. 쉘은 계속 실행됩니다.\n");
+}
+
+void handle_sigint_child(int sig) {
+    printf("\n작업 종료\n");
+    exit(0); // 자식 프로세스 종료
+}
+void handle_sigquit(int sig){
     printf("\n작업 종료\n");
 }
-
-// ----------------------------------------------------
-// SIGQUIT 핸들러 (Ctrl-Z)
-// ----------------------------------------------------
-void handle_sigquit(int sig)
-{
-    printf("\n작업 일시정지\n");
-}
-
 // ----------------------------------------------------
 // 사용자 입력을 공백으로 분리하여 인자 배열에 저장
 // ----------------------------------------------------
@@ -71,7 +69,7 @@ int getargs(char *cmd, char **argv)
 // ----------------------------------------------------
 // 명령어 실행 함수 (파일 재지향 및 파이프 처리 포함)
 // ----------------------------------------------------
-void execute_command(char **argv, int input_fd, int output_fd, int re)
+void execute_command(char **argv, int input_fd, int output_fd)
 {
     if (strcmp(argv[0], "rmdir") == 0)
     {
@@ -219,145 +217,105 @@ void execute_command(char **argv, int input_fd, int output_fd, int re)
 // ----------------------------------------------------
 // 명령어 파이프 및 파일 재지향 처리
 // ----------------------------------------------------
-void process_command(char *cmd)
-{
+void process_command(char *cmd) {
     char *commands[MAX_ARG];
     int n_commands = 0;
     int pipes[MAX_ARG][2];
     int input_fd = 0, output_fd = 1;
-    int isReOutput = 0;
 
-    // 파이프 기준으로 명령어 분리
     char *token = strtok(cmd, "|");
-    while (token != NULL)
-    {
+    while (token != NULL) {
         commands[n_commands++] = token;
         token = strtok(NULL, "|");
     }
 
-    for (int i = 0; i < n_commands; i++)
-    {
+    for (int i = 0; i < n_commands; i++) {
         char *argv[MAX_ARG];
-        char *redir_file = NULL;
+        char *redir_file;
         input_fd = (i == 0) ? 0 : pipes[i - 1][0];
         output_fd = 1;
 
-        // 파이프 생성 (다음 명령어가 있다면)
-        if (i < n_commands - 1)
-        {
+        if (i < n_commands - 1) {
             pipe(pipes[i]);
             output_fd = pipes[i][1];
         }
 
-        // 파일 출력 재지향 처리
         char *cmd_with_redir = strtok(commands[i], ">");
         redir_file = strtok(NULL, ">");
-        if (redir_file != NULL)
-        {
+        if (redir_file != NULL) {
             redir_file = strtok(redir_file, " ");
             output_fd = open(redir_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-            if (output_fd == -1)
-            {
+            if (output_fd == -1) {
                 perror("파일 열기 실패");
                 return;
             }
         }
 
-        // 파일 입력 재지향 처리
         cmd_with_redir = strtok(cmd_with_redir, "<");
         char *input_file = strtok(NULL, "<");
-        if (input_file != NULL)
-        {
+        if (input_file != NULL) {
             input_file = strtok(input_file, " ");
             input_fd = open(input_file, O_RDONLY);
-            if (input_fd == -1)
-            {
+            if (input_fd == -1) {
                 perror("파일 열기 실패");
                 return;
             }
         }
 
-        // 명령어 인자 분리
         getargs(cmd_with_redir, argv);
 
         pid_t pid = fork();
-        if (pid == 0)
-        {
-            // 자식 프로세스: 파일 디스크립터 재설정
-            if (input_fd != 0)
-            {
-                dup2(input_fd, STDIN_FILENO);
-                close(input_fd);
-            }
-            if (output_fd != 1)
-            {
-                dup2(output_fd, STDOUT_FILENO);
-                close(output_fd);
-            }
-            if (i < n_commands - 1)
-            {
-                close(pipes[i][0]); // 읽기 닫기
-            }
+        if (pid == 0) {
+            // 자식 프로세스에서 SIGINT 핸들러 설정
+            signal(SIGINT, handle_sigint_child);
 
-            // 명령 실행
-            execute_command(argv, STDIN_FILENO, STDOUT_FILENO, isReOutput);
-        }
-        else if (pid > 0)
-        {
-            // 부모 프로세스: 파일 디스크립터 닫기
-            if (input_fd != 0)
-                close(input_fd);
-            if (output_fd != 1)
-                close(output_fd);
-            if (i < n_commands - 1)
-                close(pipes[i][1]); // 쓰기 닫기
-        }
-        else
-        {
+            if (i < n_commands - 1) {
+                close(pipes[i][0]);
+            }
+            execute_command(argv, input_fd, output_fd);
+        } else if (pid > 0) {
+            // 부모 프로세스에서 SIGINT 무시
+            signal(SIGINT, handle_sigint_parent);
+
+            if (input_fd != 0) close(input_fd);
+            if (output_fd != 1) close(output_fd);
+            if (i < n_commands - 1) close(pipes[i][1]);
+
+            wait(NULL); // 자식 프로세스 종료 대기
+        } else {
             perror("포크 실패");
             return;
         }
-    }
-
-    // 모든 자식 프로세스 종료 대기
-    for (int i = 0; i < n_commands; i++)
-    {
-        wait(NULL);
     }
 }
 // ----------------------------------------------------
 // 메인 함수
 // ----------------------------------------------------
-int main()
-{
+int main() {
     char buf[MAX_BUF];
     int is_background;
 
-    signal(SIGINT, handle_sigint);
+    signal(SIGINT, handle_sigint_parent); // 부모 핸들러 설정
     signal(SIGQUIT, handle_sigquit);
 
     printf("**********커스텀 쉘 실행**********\n");
 
-    while (1)
-    {
+    while (1) {
         printf("CustomShell> ");
-        if (fgets(buf, MAX_BUF, stdin) == NULL)
-        {
+        if (fgets(buf, MAX_BUF, stdin) == NULL) {
             printf("\n");
             break;
         }
 
         buf[strcspn(buf, "\n")] = '\0';
 
-        if (strcmp(buf, "exit") == 0)
-        {
+        if (strcmp(buf, "exit") == 0) {
             printf("**********커스텀 쉘 종료**********\n");
             break;
         }
 
         is_background = 0;
-        if (buf[strlen(buf) - 1] == '&')
-        {
+        if (buf[strlen(buf) - 1] == '&') {
             is_background = 1;
             buf[strlen(buf) - 1] = '\0';
         }
